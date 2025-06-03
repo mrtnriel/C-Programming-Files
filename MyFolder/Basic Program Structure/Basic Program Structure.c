3b6445cc3bd2c8e6a6f5bfb84435f75f1ac95dbe
Martin <gabrielrmanalo@iskolarngbayan.pup.edu.ph>
2025-06-04 01:32:20 +0800

    Create Basic Program Structure.c

---
+++ MyFolder/Basic Program Structure/Basic Program Structure.c
@@ -0,0 +1,13 @@
+#include <stdio.h>  // 1. Preprocessor directive
+
+// 2. Main function
+int main() {
+    // 3. Variable declaration
+    int num = 10;
+
+    // 4. Output statement
+    printf("The number is %d\n", num);
+
+    // 5. Return statement
+    return 0;
+}
